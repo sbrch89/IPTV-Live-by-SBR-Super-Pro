6e7641e17866c7a40c390de9e73ab770f6d82fb8
sbrch89 <41589617+sbrch89@users.noreply.github.com>
2026-03-07 11:59:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -58,15 +58,13 @@ searchInput.addEventListener("input", e=>{
 const sources = [
   "https://iptv-org.github.io/iptv/index.m3u",
   "https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8",
-  "https://raw.githubusercontent.com/iptv-restream/iptv-channels/master/channels.m3u"
-  "https://iptv-org.github.io/iptv/index.m3u",
-  "https://iptv-org.github.io/iptv/languages/ara.m3u",
   "https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8",
   "https://raw.githubusercontent.com/iptv-restream/iptv-channels/master/channels.m3u"
 
 ];
 // تحميل جميع المصادر تلقائيًا
 sources.forEach(src=>loadChannelsFromM3U(src));
+
 
 
 
